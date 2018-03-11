b931bccf827cf650b672683e06f0ec2c6e2b7735
Andre Rodrigues <andre.silva96@live.com>
2018-03-11 18:12:02 -0300

    Criando observer

---
+++ app/Observers/RestaurantObserver.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Observers;
+
+class RestaurantObserver
+{
+    use UploadObserverTrait;
+
+    public function creating()
+    {
+
+    }
+
+    public function updating()
+    {
+
+    }
+    public function deleting()
+    {
+
+    }
+}
