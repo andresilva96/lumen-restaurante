4a44f20049f9d83c14101c7c804672d599f1649b
Andre Rodrigues <andre.silva96@live.com>
2018-04-12 23:05:52 -0300

    Variaveis e funcao obter url

--- app/Restaurant.php
+++ app/Restaurant.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Restaurant extends Model
 {
-    protected $fillable = ['name', 'description'];
+    protected $fillable = ['name', 'description', 'photo'];
+    protected $appends = ['photo_full_url'];
+
+    protected function getPhotoFullUrlAttribute()
+    {
+        if ($this->attributes['photo']) {
+            return 'https://s3-'.env('AWS_REGION').'.amazonaws.com/'.env('AWS_BUCKET').'/restaurant/'.$this->attributes['photo'];
+        }
+
+        return null;
+    }
 }
