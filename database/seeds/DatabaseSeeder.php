28cef7d027c3690ea347219449b21f69d8ad5a44
Andre Rodrigues <andre.silva96@live.com>
2018-06-29 00:17:02 -0300

    DB Seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call('UsersTableSeeder');
+        factory(\App\User::class)->create([
+            'email'=>'andre.silva2@live.com',
+            'password' => app('hash')->make('123456')
+        ]);
     }
 }
